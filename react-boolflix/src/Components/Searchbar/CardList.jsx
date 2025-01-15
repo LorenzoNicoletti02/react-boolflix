b31b1dffe7eed891d909cd9d9f80dfc57ce8aa37
LorenzoNicoletti02 <lorenzo.nicoletti02@gmail.com>
2025-01-15 19:17:15 +0100

    feat: stelline (non carica le stelline, necessaria revisione)

--- react-boolflix/src/Components/Searchbar/CardList.jsx
+++ react-boolflix/src/Components/Searchbar/CardList.jsx
@@ -3,16 +3,30 @@ import { contextUse } from "../../Context/GlobalContext";
 export function CardList() {
   const { data } = contextUse();
 
+  console.log(data);
   function LinguaFilm({ filmLanguage }) {
     let bandiera = "";
     if (filmLanguage === "en") {
-      bandiera = "../public/images/en.png";
+      bandiera = "./public/images/en.png";
     } else if (filmLanguage === "it") {
-      bandiera = "../public/images/it.png";
+      bandiera = "./public/images/it.png";
     } else {
-      bandiera = "../public/images/placeholder.png";
+      bandiera = "./public/images/placeholder.png";
     }
+
     return <img src={bandiera} />;
+  }
+
+  function CreaStelle({ voto }) {
+    // Arrotonda il numero per eccesso
+    const votoFinale = Math.ceil(voto / 2);
+    const stelleArray = [];
+    for (let i = 0; i < 5; i++) {
+      i < votoFinale ? stelleArray.push(`solid`) : stelleArray.push(`regular`);
+    }
+    return stelleArray.map((stelle, i) => (
+      <i key={i} className={`fa-${stelle} fa-star`}></i>
+    ));
   }
 
   return (
@@ -20,19 +34,22 @@ export function CardList() {
       {data != undefined ? (
         data.map((curElem) => (
           <div key={curElem.id}>
-            <h2>{"title" in curElem ? curElem.title : curElem.name}</h2>
-            <h3>
+            <h1>{"title" in curElem ? curElem.title : curElem.name}</h1>
+            <h2>
               {"title" in curElem
                 ? curElem.original_title
                 : curElem.original_name}
-            </h3>
-
-            <div>
+            </h2>
+            <img
+              src={`http://image.tmdb.org/t/p/w200${curElem.poster_path}`}
+              alt=""
+            />
+            <h3>
               <LinguaFilm filmLanguage={curElem.original_language} />
-            </div>
-
+              <CreaStelle voto={curElem.vote_average} />
+            </h3>
             <div>
-              Voto: {"vote_average" in curElem ? curElem.vote_average : "N/A"}
+              {"vote_average" in curElem ? curElem.vote_average : "N/A"}
             </div>
           </div>
         ))
